14cdb8912c4aecc0ade686e1f494df18a3779e12
Victor Kokado <victornerikokado@yahoo.com>
2023-03-10 16:10:32 -0300

    Conhecendo o padrão dataclass

--- micro-videos/src/category/domain/entities.py
+++ micro-videos/src/category/domain/entities.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from dataclasses import dataclass , field
+from typing import Optional
 
+@dataclass() #init,repr, eq
 class Category:
 
-    def __init__(self, name: str, description: str, is_active: bool, created_at: datetime) -> None:
-        self.name = name
-        self.description = description
-        self.is_active = is_active
-        self.created_at = created_at
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+
