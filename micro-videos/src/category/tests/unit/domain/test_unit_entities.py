8d344bf9a10ad268dcc68b86051ec3a900cc995e
Victor Kokado <victornerikokado@yahoo.com>
2023-03-23 14:16:03 -0300

    Teste propriedades da entidade categoria

--- micro-videos/src/category/tests/unit/domain/test_unit_entities.py
+++ micro-videos/src/category/tests/unit/domain/test_unit_entities.py
@@ -9,13 +9,36 @@ class TestCategoryUnit(unittest.TestCase):
         self.assertTrue(is_dataclass(Category))
 
     def test_Constructor(self):
+        category = Category(name='Movie')
+        self.assertEqual(category.name, 'Movie')
+        self.assertEqual(category.description, None )
+        self.assertEqual(category.is_active, True)
+        self.assertIsInstance(category.created_at, datetime)
+
+        created_at = datetime.now()
         category = Category(
             name='Movie', 
             description='some description', 
-            is_active=True,
-            created_at= datetime.now()
+            is_active=False,
+            created_at= created_at
         )
+
         self.assertEqual(category.name, 'Movie')
         self.assertEqual(category.description, 'some description')
-        self.assertEqual(category.is_active, True)
-        self.assertIsInstance(category.created_at, datetime)
+        self.assertEqual(category.is_active, False)
+        self.assertEqual(category.created_at, created_at)
+
+
+    def test_if_created_at_is_generated_in_constructor(self):
+        category1 = Category(name='Movie 1')
+        category2 = Category(name='Movie 2')
+
+        # Dessa forma também funciona
+        # self.assertNotEqual(
+        #     category1.created_at,
+        #     category2.created_at
+        #     )
+        self.assertNotEqual(
+            category1.created_at.timestamp(),
+            category2.created_at.timestamp()
+            )
