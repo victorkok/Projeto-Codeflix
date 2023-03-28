df734c5133cc46d4ef9ac49370bf7440e385591e
Victor Kokado <victornerikokado@yahoo.com>
2023-03-28 17:41:28 +0000

    Habilitando suporte a Git no Remote Container

---
+++ src/category/domain/entities.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from dataclasses import dataclass , field
+from typing import Optional
+
+@dataclass() #init,repr, eq dataclass
+class Category:
+
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(
+        default_factory=lambda: datetime.now()
+    )
+
+
