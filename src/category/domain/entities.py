e562dc7910386badd29070b5e31de6e9185c207a
Victor Kokado <victornerikokado@yahoo.com>
2023-03-30 17:21:12 +0000

    feat: Adding Extension in Container
    Python and Sourcery in extension on container

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,10 +1,24 @@
 from datetime import datetime
 from dataclasses import dataclass , field
 from typing import Optional
+import uuid
 
-@dataclass() #init,repr, eq dataclass
+
+#entidade - conjunto de atributos + entidades + objetos de valores | identificação + operaçoes
+
+# lapis - propriedades - color - Objeto de valor
+
+#mede,quantificar ou descrever no dominio
+#ele precisa modelar um todo conceitual com informações relacionadas,se uma unica unidade
+
+# ter que ser imutavel -> objeto de valor
+
+@dataclass(kw_only=True) #init,repr, eq dataclass
 class Category:
 
+    id: uuid.UUID = field(
+        default_factory=lambda: uuid.uuid4()
+    )
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
