2c6b29784cf6d42d35f3f982409094f70832da6c
Victor Kokado <victornerikokado@yahoo.com>
2023-04-01 19:40:26 +0000

    feat:
    Teste abstração Entity

--- src/__seedwork/tests/unit/domain/test_unit_entities.py
+++ src/__seedwork/tests/unit/domain/test_unit_entities.py
@@ -1,11 +1,15 @@
 
 from abc import ABC
-from dataclasses import is_dataclass
+from dataclasses import dataclass, is_dataclass
 import unittest
 
 from __seedwork.domain.entities import Entity
-from __seedwork.domain.value_objects import ValueObject
+from __seedwork.domain.value_objects import UniqueEntityId, ValueObject
 
+@dataclass(frozen=True, kw_only=True)
+class StubEntity(Entity):
+    prop1: str
+    prop2: str
 
 class TestEntityUnit(unittest.TestCase):
 
@@ -14,3 +18,31 @@ class TestEntityUnit(unittest.TestCase):
 
     def test_if_is_a_abstract_class(self):
         self.assertIsInstance(Entity(), ABC)
+
+    def test_set_unique_entity_id_and_props(self):
+        entity = StubEntity(prop1='value1', prop2='value2')
+        self.assertEqual(entity.prop1, 'value1')
+        self.assertEqual(entity.prop2, 'value2')
+        self.assertIsInstance(entity.unique_entity_id, UniqueEntityId)
+        self.assertEqual(entity.unique_entity_id.id, entity.id)
+
+    def test_accept_a_valid_uuid(self):
+        entity = StubEntity(
+            unique_entity_id=UniqueEntityId('10aeb3b3-12f9-4cc7-9e1c-72f4b3f03a6d'),
+            prop1='value1',
+            prop2='value2',
+        )
+
+        self.assertEqual(entity.id, '10aeb3b3-12f9-4cc7-9e1c-72f4b3f03a6d')
+
+    def test_to_dict_method(self):
+        entity = StubEntity(
+            unique_entity_id=UniqueEntityId('10aeb3b3-12f9-4cc7-9e1c-72f4b3f03a6d'),
+            prop1='value1',
+            prop2='value2',
+        )
+        self.assertDictEqual(entity.to_dict(), {
+            'id': '10aeb3b3-12f9-4cc7-9e1c-72f4b3f03a6d',
+            'prop1': 'value1',
+            'prop2': 'value2'
+        })
